f42f3bd551d8f297875346dae71c5700751b055b
darmadevZone <darmadevdone@gmail.com>
2024-02-13 18:48:46 +0900

    Githubレポジトリの定義

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/common/NetworkResponse.kt
@@ -0,0 +1,11 @@
+package jp.co.yumemi.android.code_check.common
+
+sealed class NetworkResponse<T>(
+    val data: T? = null,
+    val error: String? = null,
+) {
+
+    class Success<T>(data: T) : NetworkResponse<T>(data = data)
+    class Failure<T>(error: String) : NetworkResponse<T>(error = error)
+    class Loading<T> : NetworkResponse<T>()
+}
