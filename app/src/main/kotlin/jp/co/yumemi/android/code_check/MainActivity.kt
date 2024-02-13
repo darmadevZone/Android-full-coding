0d4e594db285973289c1c6fa08cc01e528856ba4
darmadevZone <darmadevdone@gmail.com>
2024-02-13 16:54:06 +0900

    androidEntryPointアノテーションの追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.ui.theme.YumemiCodeCheckTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
