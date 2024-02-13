86e2a73a8d79514b1f5b366edcfc3556546aa52c
darmadevZone <darmadevdone@gmail.com>
2024-02-13 18:48:20 +0900

    Githubのレスポンスの定義

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/Item.kt
@@ -0,0 +1,25 @@
+package jp.co.yumemi.android.code_check.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+
+
+///val name: String,
+//val ownerIconUrl: OwnerList,
+@JsonClass(generateAdapter = true)
+data class Item(
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "forks_count")
+    val forksCount: Int?,
+    @Json(name = "open_issues_count")
+    val openIssuesCount: Int,
+    val language: String?,
+    @Json(name = "stargazers_count")
+    val stargazersCount: Int?,
+    @Json(name = "watchers_count")
+    val watchersCount: Int?,
+    val owner: Owner,
+)
---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/SearchGithubRepositoryModel.kt
@@ -0,0 +1,14 @@
+package jp.co.yumemi.android.code_check.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SearchGithubRepositoryModel(
+    @Json(name = "incomplete_results")
+    val incompleteResults: Boolean?,
+    val items: List<Item>?,
+    @Json(name = "total_count")
+    val totalCount: Int?
+)
