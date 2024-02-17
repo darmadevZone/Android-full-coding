259047224b771c87025166e7b191d4b62dbf9a7c
darmadevZone <darmadevdone@gmail.com>
2024-02-17 20:59:08 +0900

    レポジトリのgetGithubDetailメソッドを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
@@ -1,12 +1,22 @@
 package jp.co.yumemi.android.code_check.repository
 
+import jp.co.yumemi.android.code_check.model.Item
 import jp.co.yumemi.android.code_check.model.SearchGithubRepositoryModel
 import javax.inject.Inject
 
-class GithubRepository @Inject constructor(
-    private val githubService: GithubService
+class GithubRepository
+@Inject
+constructor(
+    private val githubService: GithubService,
 ) {
     suspend fun searchGithubRepository(query: String): SearchGithubRepositoryModel {
         return githubService.searchGithubRepository(query)
+    }
+
+    suspend fun getGithubDetail(
+        owner: String,
+        name: String,
+    ): Item {
+        TODO()
     }
 }
