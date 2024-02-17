b83ada56b9c85a64f02832316badd90587a5ef2d
darmadevZone <darmadevdone@gmail.com>
2024-02-17 17:54:08 +0900

    ktlintでフォーマット

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/GithubDetailView.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/GithubDetailView.kt
@@ -1,12 +1,9 @@
 package jp.co.yumemi.android.code_check.view
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
