92fb857d5e3eebcf72dd781139ee7d25bee95b3c
darmadevZone <darmadevdone@gmail.com>
2024-02-17 21:00:58 +0900

    githubDetailViewModel関数の追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/GithubDetailView.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/GithubDetailView.kt
@@ -10,15 +10,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import jp.co.yumemi.android.code_check.ui.theme.Typography
+import jp.co.yumemi.android.code_check.viewModel.GithubDetailViewModel
 
 @Composable
-fun GithubDetailView(goToSearchView: () -> Unit) {
+fun GithubDetailView(
+    goToSearchView: () -> Unit,
+    repositoryName: String,
+    repositoryOwner: String,
+    viewModel: GithubDetailViewModel = hiltViewModel(),
+) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         AsyncImage(
             model = "https://i0.wp.com/theboreddev.com/wp-content/uploads/2021/10/kotlin_front.png?w=800&ssl=1",
@@ -28,28 +35,19 @@ fun GithubDetailView(goToSearchView: () -> Unit) {
                 .padding(vertical = 8.dp)
         )
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Text(text = "name", style = Typography.bodyLarge)
-            Text(text = "written in language", style = Typography.bodyLarge)
-            Text(text = "star count", style = Typography.bodyLarge)
-            Text(text = "watcherCount", style = Typography.bodyLarge)
-            Text(text = "forkCount", style = Typography.bodyLarge)
-            Text(text = "owner")
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    Text(text = state.data.name, style = Typography.bodyLarge)
+                    Text(text = state.data.language ?: "No Language", style = Typography.bodyLarge)
+                    Text(text = "Star Count: ${state.data.stargazersCount}", style = Typography.bodyLarge)
+                    Text(text = "watcherCount ${state.data.watchersCount}", style = Typography.bodyLarge)
+                    Text(text = "forkCount: ${state.data.forksCount}", style = Typography.bodyLarge)
+                    Text(text = "owner: ${state.data.owner}")
+                }
+            }
         }
     }
 }
-
-/**
- *
-val name: String,
-val forksCount: Int?,
-val openIssuesCount: Int,
-val language: String?,
-val stargazersCount: Int?,
-val watchersCount: Int?,
-val owner: Owner,
- */
