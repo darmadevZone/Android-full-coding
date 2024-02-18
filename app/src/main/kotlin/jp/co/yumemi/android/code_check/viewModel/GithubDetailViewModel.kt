6a09ea3c343667e86a2a5f21bec5bb914da9b080
darmadevZone <darmadevdone@gmail.com>
2024-02-18 16:36:05 +0900

    searchviewから引数をもらう処理

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/viewModel/GithubDetailViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/viewModel/GithubDetailViewModel.kt
@@ -1,7 +1,9 @@
 package jp.co.yumemi.android.code_check.viewModel
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,9 +20,18 @@ class GithubDetailViewModel
 @Inject
 constructor(
     private val searchGithubUtil: SearchGithubUtil,
+    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
     private val _state = mutableStateOf(GithubRepositoryState<Item>())
     val state: State<GithubRepositoryState<Item>> = _state
+
+    init {
+        val owner = checkNotNull(savedStateHandle.get<String>("owner"))
+        val repo = checkNotNull(savedStateHandle.get<String>("repo"))
+        Log.d("owner", owner)
+        Log.d("repo", repo)
+        getGithubDetail(repo = repo, owner = owner)
+    }
 
     fun getGithubDetail(
         repo: String,
