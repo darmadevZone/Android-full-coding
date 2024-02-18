07cf4408764f2554628fde8704fb825b9317cc3d
darmadevZone <darmadevdone@gmail.com>
2024-02-18 15:59:36 +0900

    detailのrepositoryからgithubEndpointまでの取得の実装

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubService.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubService.kt
@@ -1,10 +1,20 @@
 package jp.co.yumemi.android.code_check.repository
 
+import jp.co.yumemi.android.code_check.model.Item
 import jp.co.yumemi.android.code_check.model.SearchGithubRepositoryModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
     @GET("search/repositories")
-    suspend fun searchGithubRepository(@Query("q") query: String): SearchGithubRepositoryModel
+    suspend fun searchGithubRepository(
+        @Query("q") query: String,
+    ): SearchGithubRepositoryModel
+
+    @GET("repos/{owner}/{repo}")
+    suspend fun getGithubDetail(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+    ): Item
 }
