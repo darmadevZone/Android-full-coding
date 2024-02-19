6a2c99c8cb1f6254afaf1dc41393ebbe26e1294f
darmadevZone <darmadevdone@gmail.com>
2024-02-19 13:27:28 +0900

    github detail viewのUiをブラッシュアップ

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/GithubDetailView.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/GithubDetailView.kt
@@ -4,10 +4,18 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,6 +27,7 @@ import coil.compose.AsyncImage
 import jp.co.yumemi.android.code_check.ui.theme.Typography
 import jp.co.yumemi.android.code_check.viewModel.GithubDetailViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GithubDetailView(
     goToSearchView: () -> Unit,
@@ -42,26 +51,74 @@ fun GithubDetailView(
             }
 
             state.data != null -> {
-                AsyncImage(
-                    model = state.data.owner.avatarUrl,
-                    contentDescription = state.data.name,
-                    modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                )
-
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    Text(text = state.data.name, style = Typography.bodyLarge)
-                    Text(text = state.data.language ?: "No Language", style = Typography.bodyLarge)
-                    Text(text = "Star Count: ${state.data.stargazersCount}", style = Typography.bodyLarge)
-                    Text(text = "watcherCount ${state.data.watchersCount}", style = Typography.bodyLarge)
-                    Text(text = "forkCount: ${state.data.forksCount}", style = Typography.bodyLarge)
-                    Text(text = "owner: ${state.data.owner}")
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(text = state.data.owner.name)
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { goToSearchView() }) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = "戻る",
+                                    )
+                                }
+                            },
+                        )
+                    },
+                ) { inner ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(inner),
+                    ) {
+                        AsyncImage(
+                            model = state.data.owner.avatarUrl,
+                            contentDescription = state.data.name,
+                            modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp),
+                        )
+                        Column(
+                            verticalArrangement =
+                                Arrangement.spacedBy(
+                                    20.dp,
+                                    Alignment.CenterVertically,
+                                ),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.fillMaxSize(),
+                        ) {
+                            Text(
+                                text = "${state.data.owner.name}/${state.data.name}",
+                                style = Typography.bodyLarge,
+                            )
+                            Spacer(modifier = Modifier)
+                            val language = state.data.language ?: "No Language"
+                            Text(
+                                text = "programming language: $language",
+                                style = Typography.bodyLarge,
+                            )
+                            Spacer(modifier = Modifier)
+                            Text(
+                                text = "Star Count: ${state.data.stargazersCount}",
+                                style = Typography.bodyLarge,
+                            )
+                            Spacer(modifier = Modifier)
+                            Text(
+                                text = "watcherCount ${state.data.watchersCount}",
+                                style = Typography.bodyLarge,
+                            )
+                            Spacer(modifier = Modifier)
+                            Text(
+                                text = "forkCount: ${state.data.forksCount}",
+                                style = Typography.bodyLarge,
+                            )
+                        }
+                    }
                 }
             }
         }
